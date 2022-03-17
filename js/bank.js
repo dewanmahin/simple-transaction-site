256574b0e916062e6c858cc4c1b14898defe47ea
DEWAN <dewanmahinchy@gmail.com>
2022-03-17 21:00:36 +0600

    bank page ui done

--- js/bank.js
+++ js/bank.js
@@ -1,3 +1,11 @@
 document.getElementById('submit-btn').addEventListener('click', function(){
-    console.log('button clicked')
+    const userEmail = document.getElementById('user-email').value;
+    const userPass = document.getElementById('user-pass').value;
+
+    if(userEmail == 'owner@bank.com' && userPass == 'bankPass'){
+        // Redirect to another html page on button click
+        window.location.href = 'bank.html'
+    }else{
+        alert('Enter your correct information.')
+    }
 })
