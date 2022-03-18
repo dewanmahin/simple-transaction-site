13ab6a57ab719544fd86735ffb743149a709f683
DEWAN <dewanmahinchy@gmail.com>
2022-03-18 22:58:11 +0600

    file name & location has changed

---
+++ js/login.js
@@ -0,0 +1,11 @@
+document.getElementById('submit-btn').addEventListener('click', function(){
+    const userEmail = document.getElementById('user-email').value;
+    const userPass = document.getElementById('user-pass').value;
+
+    if(userEmail == 'owner@bank.com' && userPass == 'bankPass'){
+        // Redirect to another html page on button click
+        window.location.href = 'bank.html'
+    }else{
+        alert('Enter your correct information.')
+    }
+})
