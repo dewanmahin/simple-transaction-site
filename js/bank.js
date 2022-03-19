35325e632edf4d3bca37705a3762f4836c018ada
DEWAN <dewanmahinchy@gmail.com>
2022-03-19 22:29:00 +0600

    withdraw feature done

--- js/bank.js
+++ js/bank.js
@@ -19,3 +19,25 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     // clear the deposit input field
     depositInput.value = '';
 })
+
+//! Handle withdraw button event
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmount = withdrawInput.value;
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotal = withdrawTotal.innerText;
+    const newWithdrawTotal = parseFloat(previousWithdrawTotal) + parseFloat(withdrawAmount);
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotal.innerText;
+    const newBalanceTotal = parseFloat(previousBalanceTotal) - parseFloat(withdrawAmount);
+
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear the withdraw input field
+    withdrawInput.value = '';
+})
