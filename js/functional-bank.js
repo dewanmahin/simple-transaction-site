4c2e4e9f43957805fe9e0861a07115a2da444e57
DEWAN <dewanmahinchy@gmail.com>
2022-03-23 23:17:49 +0600

    balance total update feature added

--- js/functional-bank.js
+++ js/functional-bank.js
@@ -1,4 +1,4 @@
-//! getInputValue() use for collect all input value
+// getInputValue() use for collect all input value
 function getInputValue(inputId){
     const inputField = document.getElementById(inputId);
     const inputFieldText = inputField.value;
@@ -7,15 +7,25 @@ function getInputValue(inputId){
     inputField.value = ''
     return inputAmount;
 }
-
-//! updateTotal() use for update balance and transactions details
+// updateTotal() use for update balance and transactions details
 function updateTotal(totalId, amount){
     const total = document.getElementById(totalId);
     const totalText = total.innerText;
     const totalAmount = parseFloat(totalText);
-
     // Total all amount
     total.innerText = totalAmount + amount;
+}
+// balanceUpdate() update present account balance
+function balanceUpdate(amount, isAdd){
+    const balanceTotal = document.getElementById("balance-total");
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    if(isAdd == true){
+        balanceTotal.innerText = previousBalanceTotal + amount;
+    }else{
+        balanceTotal.innerText = previousBalanceTotal - amount;
+    }
 }
 
 //! Deposit button handler
@@ -23,6 +33,8 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     const depositAmount = getInputValue("deposit-input")
 
     updateTotal('deposit-total', depositAmount)
+
+    balanceUpdate(depositAmount, true);
 })
 
 //! Withdraw button handler
@@ -30,4 +42,6 @@ document.getElementById('withdraw-btn').addEventListener('click', function(){
     const withdrawAmount = getInputValue('withdraw-input');
 
     updateTotal('withdraw-total', withdrawAmount)
+
+    balanceUpdate(withdrawAmount, false);
 })
